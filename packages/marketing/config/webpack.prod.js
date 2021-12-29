b227603463f2fe2dd5db232bd33759082d4c8988
Cris Ryan Tan <crisryantan@gmail.com>
2021-12-29 17:22:27 +1100

    webpack fixes

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing/latest/'
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
