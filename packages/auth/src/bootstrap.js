73aab316e36ea205149e1d817263bd484a012998
Cris Ryan Tan <crisryantan@gmail.com>
2022-01-02 15:52:05 +1100

    add auth package starter files

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  // if authApp is ran in isolation
+  if(onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+
+      // prevent infinite loop when navigating
+      if(pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, {
+      // browser history in isolation
+      defaultHistory: createBrowserHistory(),
+    });
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
